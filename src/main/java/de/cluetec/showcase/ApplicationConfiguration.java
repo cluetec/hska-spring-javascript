daf212ebb01e48feb5f1a6272f464eabf9f907b2
iab <iab@cluetec.de>
2015-06-09 15:03:05 +0200

    Add main class and application configuration

---
+++ src/main/java/de/cluetec/showcase/ApplicationConfiguration.java
@@ -0,0 +1,12 @@
+package de.cluetec.showcase;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class ApplicationConfiguration {
+
+}
